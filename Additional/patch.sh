a55213c77780059f6696f6d68ce47b32a4b9e89f
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2021-07-20 20:29:57 -0700

    Add files via upload

---
+++ Additional/patch.sh
@@ -0,0 +1,51 @@
+bbox(){
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo -e "\e[92m            MASTER-BOMBER 2.O NEW PATCH V1.1 IS AVAILABLE\e[0m"
+echo -e "\e[96m         ╔═══════════════════════════════════════════════════╗\e[0m"
+echo -e "\e[96m         ║     \e[93mMaster-Bomber 2.O\e[96m Patch Is available\e[93m V1.1\e[96m     ║\e[0m"
+echo -e "\e[96m         ║       To Patch The \e[93mMaster-Bomber 2.O\e[96m Tool         ║\e[0m"
+echo -e "\e[96m         ║                                                   ║\e[0m"
+echo -e "\e[96m         ║               Press \e[92mEnter\e[96m to Patch\e[96m                ║\e[0m"
+echo -e "\e[96m         ╚═══════════════════════════════════════════════════╝\e[0m"
+echo
+echo
+echo
+echo
+read -n1 -r -p  " " key
+
+
+clear
+echo -e "\e[92m "
+clear
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[36m Requesting Solve Bugs From Source...\e[93m"
+sleep 2
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[0m Request Attempt Successful..\e[93m"
+sleep 1
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[36m Solving Now...\e[93m"
+sleep 2
+echo ""
+cd ..
+rm -rf m-bomber2.O
+git clone https://github.com/jatinkalwar/m-bomber2.O > /dev/null 2>&1
+cd m-bomber2.O
+
+clear
+echo -e "                   \e[96mREBOOTING MASTER-BOMBER 2.O......!\e[0m"
+sleep 4.0
+cd $HOME/m-bomber2.O
+bash bomber.sh
+echo
+fi
+}
+bbox
---
+++ Additional/update.sh
@@ -0,0 +1,52 @@
+
+dbox(){
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo -e "\e[92m            MASTER-BOMBER 2.O NEW UPDATE V1.1 IS AVAILABLE\e[0m"
+echo -e "\e[96m         ╔═══════════════════════════════════════════════════╗\e[0m"
+echo -e "\e[96m         ║     \e[93mMaster-Bomber 2.O\e[96m Update Is available\e[93m V1.1\e[96m    ║\e[0m"
+echo -e "\e[96m         ║       To Update The \e[93mMaster-Bomber 2.O\e[96m Tool        ║\e[0m"
+echo -e "\e[96m         ║                                                   ║\e[0m"
+echo -e "\e[96m         ║               Press \e[92menter\e[96m to update\e[96m               ║\e[0m"
+echo -e "\e[96m         ╚═══════════════════════════════════════════════════╝\e[0m"
+echo
+echo
+echo
+echo
+read -n1 -r -p  " " key
+
+
+clear
+echo -e "\e[92m "
+clear
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[36m Requesting Update From Source...\e[93m"
+sleep 2
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[0m Request Attempt Successful..\e[93m"
+sleep 1
+echo ""
+echo -e "      \e[92m[\e[93m~\e[92m]\e[36m Updating Now...\e[93m"
+sleep 2
+echo ""
+cd ..
+rm -rf m-bomber2.O
+git clone https://github.com/jatinkalwar/m-bomber2.O > /dev/null 2>&1
+cd m-bomber2.O
+
+clear
+echo ""
+echo ""
+echo -e "     \e[92m[\e[91m~\e[92m]\e[91m Restarting MASTER-BOMBER 2.O !\e[93m"
+sleep 2
+bash bomber.sh
+clear
+}
+dbox
