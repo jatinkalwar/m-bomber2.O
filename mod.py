8e5c491391adea360fa89366602d5c42a3395e42
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2023-08-22 19:55:11 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -2,10 +2,10 @@ import os
 try:
     import aiohttp
 except Exception:
-    os.system("python -m pip install aiohttp")
+    os.system("python3 -m pip install aiohttp")
 
 try:
     import requests
 except Exception:
-    os.system("python -m pip install requests")
+    os.system("python3 -m pip install requests")
     
